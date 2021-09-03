55996cbcee22a12e2c2822cd01a98e9846b6c124
VitorMartin <vitor.martin.simoni@gmail.com>
2021-09-03 01:44:02 -0300

    Scraper para os sprites

---
+++ data/scraper/pokemon_sprites_scrapper.py
@@ -0,0 +1,9 @@
+import requests
+
+base_url_all_pkms = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/%ID%.png'
+
+if __name__ == '__main__':
+    for i in range(1, 152):
+        url = base_url_all_pkms.replace('%ID%', str(i))
+        pkm_sprite = requests.get(url=url)
+        open(f'../pokemons/sprites/{i}.png', 'wb').write(pkm_sprite.content)
