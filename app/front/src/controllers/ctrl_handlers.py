e751768e7ab732d82489dd3fc34048b602dd9e93
BRUNA  GALASTRI GUEDES <18.00189-0@maua.br>
2021-09-29 17:43:09 -0300

    Começando MVC no front

---
+++ app/front/src/controllers/ctrl_handlers.py
@@ -0,0 +1,9 @@
+from app.front.src.models.i_dispositivo import I_Dispositivo
+
+class Ctrl_Handlers:
+
+        def __init__(self):
+            pass
+
+        def assign_handler(self, dispositivo: I_Dispositivo):
+            pass
--- app/front/src/main_front.py
+++ app/front/src/main_front.py
@@ -1,4 +1,10 @@
+from app.back.src.main_back import Main_Back
+
 class Main_Front:
+
+    backend = Main_Back()
+
     def __init__(self):
         print('Front iniciado...')
-        pass
+        
+
